da94918b103d6e1892efeb82aad42de5244d0003
SandeepRajanP <39520230+SandeepRajanP@users.noreply.github.com>
2018-06-17 04:13:18 +0530

    Bug Fix

--- Problem4.sh
+++ Problem4.sh
@@ -24,7 +24,7 @@ done
 aws ec2 run-instances --image-id ami-14c5486b --instance-type t2.micro --key-name FirstKeyPair --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ServerInstanceSandeep},{Key=username,Value=SandeepRajanP},{Key=emailId,Value=sandeeprajan.p@quantiphi.com},{Key=Project,Value=PE_Training}]' --iam-instance-profile Arn=arn:aws:iam::488599217855:instance-profile/PE_trainee_Admin_role --user-data '#!/bin/bash
 yum update -y
 aws s3 cp s3://buckettraining007/id_rsa.pub /home/ec2-user/.ssh/
-chmod 700 chmod 700 /home/ec2-user/.ssh
+chmod 700 /home/ec2-user/.ssh
 chmod 600 /home/ec2-user/.ssh/id_rsa.pub
 chmod 600 /home/ec2-user/.ssh/authorized_keys	
 cat /home/ec2-user/.ssh/id_rsa.pub >> /home/ec2-user/.ssh/authorized_keys' --region us-east-1 --security-group-ids sg-7d965f36
